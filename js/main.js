ec66eb6c7ab4a124c7d2afbe70e5b9363dbaf01d
Mario <nicolasmariogirotto@gmail.com>
2022-05-26 17:28:08 -0300

    Que onda esto?

---
+++ js/main.js
@@ -0,0 +1,33 @@
+var headCodeBlock = '<meta charset="UTF-8">'+
+                        '<meta http-equiv="X-UA-Compatible" content="IE=edge">'+
+                        '<meta name="viewport" content="width=device-width, initial-scale=1.0">'+
+                        '<title>Pastelería Codo a codo</title>'+                        
+                        '<link rel="shortcut icon" href="./Imagenes/icono.png" type="image/x-icon">'+
+                        '<link rel="preconnect" href="https://fonts.googleapis.com">'+
+                        '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>'+
+                        '<link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300&family=Roboto+Slab:wght@300&display=swap" rel="stylesheet">'+
+                        '<link rel="stylesheet" href="./css/main.css">';
+
+document.querySelector("head").innerHTML = headCodeBlock                    
+
+var headerCodeBlock = '<nav class="box">'+
+                        '<a class="nav" href="index.html">Home</a>'+
+                        '<a class="nav" href="acercade.html">Nosotros</a>'+
+                        '<a class="nav" href="contacto.html">Contacto</a>'+
+                        '<a class="nav" href="recetas.html">Recetas</a>'+
+                        '<a class="nav" href="registro.html">Registro</a>'+
+                        '<a class="nav" href="sucursales.html" title="Sucursales">Sucursales</a>'+
+                        '</nav>'+
+                        '<h1>Pasteleria Codo a codo</h1>';
+
+var footerCodeBlock = '<nav>'+
+                        '<a class="socials" href="https://www.facebook.com" target="_blank"><i class="fa-brands fa-facebook-square fa-2x"></i></a>'+
+                        '<a class="socials" href="https://www.twitter.com" target="_blank"><i class="fa-brands fa-twitter-square fa-2x"></i></a>'+
+                        '<a class="socials" href="https://www.instagram.com" target="_blank"><i class="fa-brands fa-instagram-square fa-2x"></i></a>'+
+                        '</nav>'+
+                        '<p>Derechos reservados 2022</p>';
+
+// Inserting the code block to wrapper element
+
+document.querySelector("header").innerHTML = headerCodeBlock
+document.querySelector("footer").innerHTML = footerCodeBlock
